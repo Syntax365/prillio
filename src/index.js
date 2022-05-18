873d17d3d0736b4f492540dfed8e843f5eafa9a5
Syntax365 <synxtax365@gmail.com>
2022-05-18 17:07:46 -0400

    Initialization Express App

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.get("*", (req, res) => {
+  res.send("Panda is a penis devil");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port 8080");
+});
