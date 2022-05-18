b207f069893b734fcf79bfe053948f505ee8b962
Syntax365 <synxtax365@gmail.com>
2022-05-18 18:06:20 -0400

    Update Port Config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get("*", (req, res) => {
   res.send("Panda is a test devil");
 });
 
 app.listen(port, () => {
-  console.log("Listening to port 8080");
+  console.log("Listening to port 3000");
 });
